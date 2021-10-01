8844a5745c4f24d95eb7330833c7e25aa7ba1093
Madara <madara.bieza@gmail.com>
2021-10-01 20:32:42 +0300

    Added API documentation URL

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -12,6 +12,8 @@ export default function Dictionary() {
     function search(event) {
         event.preventDefault();
 
+        //Documentation: https://dictionaryapi.dev/
+
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
         console.log(apiUrl);
