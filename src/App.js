29e9fe5f76c3ddcc784d74064507cff0d008a3ce
Madara <madara.bieza@gmail.com>
2021-10-03 16:08:13 +0300

    Added Phonetyc

--- src/App.js
+++ src/App.js
@@ -8,17 +8,15 @@ export default function App() {
         <header className="App-header">
           <h1>DICTIONARY</h1>
           <h4>What word are you looking for?</h4>
-          <main>
-          <Dictionary />
-          </main>
         </header>
-        <footer className="text-center">
-          <div className="git-hub">
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
             <p>Created by Madara Biezā</p>
             <a href="https://github.com/madara-bie/dictionary-project" target="_blank" rel="noreferrer">
               Open the source code
             </a>
-        </div>
         </footer>
       </div>
     </div>
